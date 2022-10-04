4e0a418a6d525041767a6f2db5b284c6dc4014fe
unknown <emmysoetan@gmail.com>
2022-10-04 08:00:29 +0100

    added footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,17 @@
 const Contact = () => {
     return (  
         <div>
-            This is the contact section
+        <h3 className="text-2xl font-poppins font-medium text-center mt-14 mb-5 sm:text-4xl">
+            Send me an email
+        </h3>
+         <form action="" method="post" className="flex flex-col w-96 mx-auto sm:w-2/5">
+            <input type="text" className="m-2 p-2 outline-none border-b-2 border-blue-500 font-poppins bg-inherit" placeholder="Fullname"/>
+            <input type="email" name="" id="" className="font-poppins m-2 p-2 outline-none border-b-2 border-blue-500 bg-inherit" placeholder="Your email"/>
+            <textarea name="" id="" cols="30" rows="10" className="m-2 p-2 outline-none border-b-2 border-blue-500 bg-inherit">
+
+            </textarea>
+            <button type='submit' className="bg-blue-500 text-slate-100 font-poppins m-2 p-2 rounded-full hover:scale-95"> SEND</button>
+         </form>
         </div>
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
+import { 
+    FaLinkedin, 
+    FaGithub,
+    FaTwitterSquare
+} from "react-icons/fa";
+
+
 const Footer = () => {
     return ( 
-        <div>
-            This is the footer section
+        <div className='' >
+            <div className="p-4 mt-10 mx-4 border-t-2 border-slate-300">
+                <h1 className="text-2xl font-poppins text-gray-800 mx-2">Soetan.dev</h1>
+                <div className="text-2xl text-gray-700 p-2 flex gap-6 sm:text-3xl">
+                <a href=""> <FaLinkedin/></a>
+                <a href=""> <FaGithub/> </a>
+                <a href=""> <FaTwitterSquare/> </a>
+            </div>
+            </div>
+            
         </div>
      );
 }
