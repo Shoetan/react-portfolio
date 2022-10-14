4bdffd148ebed12cee60480a34c2d6c290f24aa2
Immanuel <emmysoetan@gmail.com>
2022-10-14 03:02:12 +0100

    Modified navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,18 +8,16 @@ const Navbar = () => {
 
             <div className="">
                 <ul className="hidden sm:flex font-poppins text-xl font-light gap-4 mt-3">
-                    <li><a className="bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-50 px-4 py-2 rounded-md hover:opacity-75 "href="">About</a></li>
 
                     <li><a className="bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-50 px-4 py-2 rounded-md hover:opacity-75"href="">Projects</a></li>
 
                     <li><a className="bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-50 px-4 py-2 rounded-md hover:opacity-75"href="">Contact</a></li>
 
-                    <li><a className="text-blue-400 px-4 py-2 rounded-md hover:opacity-75 border border-blue-500/40"href="">Resume</a></li>
+                    <li><a className="text-blue-400 px-4 py-2 rounded-md hover:opacity-75 border border-blue-500/40"href="" download>Resume</a></li>
                 </ul>
+                
+                <a className="text-blue-400 px-4 py-2 rounded-md hover:opacity-75 border border-blue-500/40"href="" download>Resume</a>
 
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" className="text-2xl sm:hidden" />
-                    </svg>
             </div>
     
            </nav>
